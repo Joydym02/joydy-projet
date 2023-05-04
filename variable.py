a50b20688bc66631e6ef7e573d43728f12a2f009
joydy mwamba <joydym02@gmail.com>
2023-05-04 10:19:36 +0200

    mon premier projet

---
+++ variable.py
@@ -0,0 +1,13 @@
+print(type(nb1))
+#declaration de variable de type entier
+nb1=4
+nb2=5
+print(nb1+nb2)
+#declaration de variable de virgule flottante
+nb3=10.5
+Nb4=-3.1
+SOMME=nb3+Nb4
+print(SOMME)
+#declaration de variable de chaine de caractere
+nom="joydy"
+print(nom)
